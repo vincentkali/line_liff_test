bc344ee1d7603cb8999d817e266ddad84f6f459f
vincent <35392756+vincentkali@users.noreply.github.com>
2022-09-30 23:21:32 +0800

    Add files via upload

---
+++ liff.js
@@ -0,0 +1,15 @@
+var liffID = '1657480183-gEAmDPN5';
+
+liff.init({
+    liffId: liffID
+}).then(function () {
+    console.log('LIFF init');
+
+    // 這邊開始寫使用其他功能
+    context = liff.getContext();
+    console.log(context)
+
+
+}).catch(function (error) {
+    console.log(error);
+});
